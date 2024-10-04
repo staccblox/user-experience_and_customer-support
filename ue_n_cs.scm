2f3eee2f316fe213d2c45fb46eb54f4f47b3d5cc
staccblox <67884277+staccblox@users.noreply.github.com>
2024-10-04 15:20:12 -0400

    Update ue_n_cs.scm

--- ue_n_cs.scm
+++ ue_n_cs.scm
@@ -1,3 +1,5 @@
 (begin
   (display "how easy a product is to use, how efficient it is, and how useful it is; making sure customers are successful in solving whatever issues they came to your business to help solve.")
-  (newline))
+  (newline)
+  (display "An emotion wheel can be useful for approximating things.") 
+ )
